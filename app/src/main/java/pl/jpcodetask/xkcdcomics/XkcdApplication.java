1813ee8f0bdcea4a917caf8164d1115762d6d500
Jakub Pruszyński <jpcodetask@gmail.com>
2019-03-13 18:11:15 +0100

    override onCreate in XkcdApplication and build AppComponent

--- app/src/main/java/pl/jpcodetask/xkcdcomics/XkcdApplication.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/XkcdApplication.java
@@ -2,5 +2,16 @@ package pl.jpcodetask.xkcdcomics;
 
 import android.app.Application;
 
+import pl.jpcodetask.xkcdcomics.di.DaggerAppComponent;
+
 public class XkcdApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DaggerAppComponent.builder()
+                .application(this)
+                .build()
+                .inject(this);
+    }
 }
