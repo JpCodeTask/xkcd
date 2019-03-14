225a00d520751ae290af66d90d9a7f331425a8c7
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 10:25:27 +0100

    fix bug missing contributeMainActivityInjector in ActivityBuilderModule

--- app/src/main/java/pl/jpcodetask/xkcdcomics/di/ActivityBuilderModule.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/di/ActivityBuilderModule.java
@@ -1,8 +1,14 @@
 package pl.jpcodetask.xkcdcomics.di;
 
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+import pl.jpcodetask.xkcdcomics.main.MainActivity;
 
 @Module
 abstract class ActivityBuilderModule {
 
+    @ContributesAndroidInjector(
+            modules = {FragmentBuilderModule.class}
+    )
+    abstract MainActivity contributeMainActivityInjector();
 }
