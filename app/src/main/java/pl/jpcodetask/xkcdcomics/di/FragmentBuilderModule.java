fcc47bd704dbca858203083cbe1c155fe58b2ac5
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 10:24:38 +0100

    add contributeComicListFragmentInjectorr in FragmentBuilderModule

--- app/src/main/java/pl/jpcodetask/xkcdcomics/di/FragmentBuilderModule.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/di/FragmentBuilderModule.java
@@ -1,8 +1,12 @@
 package pl.jpcodetask.xkcdcomics.di;
 
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+import pl.jpcodetask.xkcdcomics.main.ComicListFragment;
 
 @Module
 abstract class FragmentBuilderModule {
 
+    @ContributesAndroidInjector
+    abstract ComicListFragment contributeComicListFragmentInjector();
 }
