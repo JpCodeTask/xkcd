11879222129da6cbaf075afded74bcf2cad67a45
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 13:45:53 +0100

    create ComicsAdapter and ComicViewHolder wwithout implementation

--- app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
@@ -8,9 +8,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import dagger.android.support.AndroidSupportInjection;
 import pl.jpcodetask.xkcdcomics.R;
+import pl.jpcodetask.xkcdcomics.databinding.FragmentComicListBinding;
 
 public class ComicListFragment extends Fragment {
 
@@ -18,7 +21,7 @@ public class ComicListFragment extends Fragment {
         //empty
     }
 
-    public static ComicListFragment newInstance(){
+    static ComicListFragment newInstance(){
         return new ComicListFragment();
     }
 
@@ -31,6 +34,44 @@ public class ComicListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_comic_list, container, false);
+        FragmentComicListBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_comic_list, container, false);
+        return binding.getRoot();
     }
+
+    private static class ComicsAdapter extends RecyclerView.Adapter<ComicsAdapter.ComicViewHolder>{
+
+
+        ComicsAdapter(){
+
+        }
+
+        @NonNull
+        @Override
+        public ComicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ComicViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        private static class ComicViewHolder extends RecyclerView.ViewHolder{
+
+            public ComicViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+
+            public void bind(){
+
+            }
+        }
+    }
+
+
 }
