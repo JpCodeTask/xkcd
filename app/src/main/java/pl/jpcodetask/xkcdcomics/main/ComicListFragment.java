22ef6e22a5237721595cb42efcb6421e681893b9
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 09:19:08 +0100

    create required empty constructor and add method newInstance

--- app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
@@ -4,4 +4,11 @@ import androidx.fragment.app.Fragment;
 
 public class ComicListFragment extends Fragment {
 
+    public ComicListFragment(){
+        //empty
+    }
+
+    public static ComicListFragment newInstance(){
+        return new ComicListFragment();
+    }
 }
