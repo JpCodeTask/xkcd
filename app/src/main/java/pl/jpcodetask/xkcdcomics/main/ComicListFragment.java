e18009e5cea7a8decdfe042494b957d0e0c80c43
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 10:23:50 +0100

    overide onAttach and onCreateView method create layout fragment_comic_list

--- app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/main/ComicListFragment.java
@@ -1,6 +1,15 @@
 package pl.jpcodetask.xkcdcomics.main;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import pl.jpcodetask.xkcdcomics.R;
 
 public class ComicListFragment extends Fragment {
 
@@ -10,5 +19,16 @@ public class ComicListFragment extends Fragment {
 
     public static ComicListFragment newInstance(){
         return new ComicListFragment();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_comic_list, container, false);
     }
 }
