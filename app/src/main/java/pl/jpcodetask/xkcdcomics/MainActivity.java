661760485072e0e0044a2ef64b2ac9199c8b5321
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-14 09:13:23 +0100

    implement HasSupportFragmentInjector in MainActivity

--- app/src/main/java/pl/jpcodetask/xkcdcomics/MainActivity.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/MainActivity.java
@@ -2,13 +2,29 @@ package pl.jpcodetask.xkcdcomics;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import javax.inject.Inject;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> mFragmentDispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return mFragmentDispatchingAndroidInjector;
     }
 }
