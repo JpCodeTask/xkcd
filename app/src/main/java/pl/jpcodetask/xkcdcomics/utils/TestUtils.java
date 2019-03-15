61f9676db1a3b109d1a317b3b0e9f207c26fce34
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-15 09:54:22 +0100

    create TestUtils class with method getData

---
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/utils/TestUtils.java
@@ -0,0 +1,21 @@
+package pl.jpcodetask.xkcdcomics.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class TestUtils {
+
+    public static List<String> getData(){
+        List<String> data = new ArrayList<>();
+        data.add("Lorem ipsum 1");
+        data.add("Lorem ipsum 2");
+        data.add("Lorem ipsum 3");
+        data.add("Lorem ipsum 4");
+        data.add("Lorem ipsum 5");
+        data.add("Lorem ipsum 6");
+        data.add("Lorem ipsum 7");
+        data.add("Lorem ipsum 8");
+
+        return data;
+    }
+}
