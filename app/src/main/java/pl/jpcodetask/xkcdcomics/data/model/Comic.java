adedb8918fd6f2297ca390aac086f0015300bf07
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-15 14:02:20 +0100

    move Comic class from data package to data.model package

--- app/src/main/java/pl/jpcodetask/xkcdcomics/data/model/Comic.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/data/model/Comic.java
@@ -1,4 +1,4 @@
-package pl.jpcodetask.xkcdcomics.data;
+package pl.jpcodetask.xkcdcomics.data.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
