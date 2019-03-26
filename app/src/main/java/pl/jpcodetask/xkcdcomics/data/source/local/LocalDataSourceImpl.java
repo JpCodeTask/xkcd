2098f58672f860d76687d5f4eae7894d41a14c32
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-03-26 12:10:38 +0100

    setup clearing table after each app's opening

--- app/src/main/java/pl/jpcodetask/xkcdcomics/data/source/local/LocalDataSourceImpl.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/data/source/local/LocalDataSourceImpl.java
@@ -2,8 +2,10 @@ package pl.jpcodetask.xkcdcomics.data.source.local;
 
 import java.util.List;
 
+import io.reactivex.CompletableObserver;
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.disposables.Disposable;
 import pl.jpcodetask.xkcdcomics.data.model.Comic;
 import pl.jpcodetask.xkcdcomics.data.source.DataSource;
 import pl.jpcodetask.xkcdcomics.utils.Schedulers;
@@ -15,18 +17,38 @@ public class LocalDataSourceImpl implements DataSource {
 
     public LocalDataSourceImpl(ComicDao comicDao){
         mComicDao = comicDao;
-        for(int i = 0; i < 10; i++){
-            Comic comic = new Comic();
-            comic.setNum(i);
-            comic.setTitle("Lorem ipsum 220122" + i);
-            comic.setDay(i);
-            comic.setMonth(i + 2);
-            comic.setYear(10 * i + 1000);
 
-            mComicDao.insert(comic)
-                    .subscribeOn(Schedulers.io())
-                    .subscribe();
-        }
+        mComicDao.removeAll()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        for(int i = 0; i < 10; i++){
+                            Comic comic = new Comic();
+                            comic.setNum(i + 2000);
+                            comic.setTitle("Lorem ipsum 220122" + i);
+                            comic.setDay(i);
+                            comic.setMonth(i + 2);
+                            comic.setYear(10 * i + 1000);
+
+                            mComicDao.insert(comic)
+                                    .subscribeOn(Schedulers.io())
+                                    .subscribe();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+
+
     }
 
 
