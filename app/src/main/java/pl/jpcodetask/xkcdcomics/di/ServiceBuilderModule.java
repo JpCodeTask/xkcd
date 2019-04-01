aad8588783899050e90ee33cdf760840e452e5a0
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-04-01 10:32:53 +0200

    remove XkcdService

--- app/src/main/java/pl/jpcodetask/xkcdcomics/di/ServiceBuilderModule.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/di/ServiceBuilderModule.java
@@ -1,12 +1,10 @@
 package pl.jpcodetask.xkcdcomics.di;
 
 import dagger.Module;
-import dagger.android.ContributesAndroidInjector;
-import pl.jpcodetask.xkcdcomics.utils.XkcdService;
 
 @Module
 abstract class ServiceBuilderModule {
 
-    @ContributesAndroidInjector
-    abstract XkcdService contributeXkcdService();
+    /*@ContributesAndroidInjector
+    abstract XkcdService contributeXkcdService();*/
 }
