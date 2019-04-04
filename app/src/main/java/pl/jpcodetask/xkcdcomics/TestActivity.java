5aad2345ef0837a33a09085013025675884558b1
Jakub Pruszyński <jpcodetask@gmail.com>
2019-04-04 19:26:07 +0200

    test4

--- app/src/main/java/pl/jpcodetask/xkcdcomics/TestActivity.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/TestActivity.java
@@ -1,8 +1,10 @@
 package pl.jpcodetask.xkcdcomics;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
