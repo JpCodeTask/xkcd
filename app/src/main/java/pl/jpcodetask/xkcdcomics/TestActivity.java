31ffe793d942d71115c4a8c26d9c04a4483440e9
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-04-04 15:16:41 +0200

    test 3

---
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/TestActivity.java
@@ -0,0 +1,14 @@
+package pl.jpcodetask.xkcdcomics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
