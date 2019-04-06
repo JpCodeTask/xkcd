b4cec3f183e49bc169c4696f6e12d5d506870776
Jakub Pruszyński <jpcodetask@gmail.com>
2019-04-06 12:21:11 +0200

    setup loading basic comic's data

--- app/src/main/java/pl/jpcodetask/xkcdcomics/ui/ComicFragment.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/ui/ComicFragment.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import dagger.android.support.AndroidSupportInjection;
 import pl.jpcodetask.xkcdcomics.databinding.FragmentComicBinding;
@@ -35,11 +38,26 @@ public class ComicFragment extends Fragment {
         super.onAttach(context);
     }
 
+
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentComicBinding binding = FragmentComicBinding.inflate(inflater, container, false);
+
         mViewModel = ((ViewModelProvider<ComicViewModel>) Objects.requireNonNull(getActivity())).obtainViewModel(getActivity());
-        mViewModel.getComicLiveData().observe(this, comic -> Toast.makeText(getContext(), "" + comic.getNum(), Toast.LENGTH_SHORT).show());
+        mViewModel.getIsDataLoading().observe(this, isDataLoading ->{
+            if(isDataLoading){
+                binding.progressBar.setVisibility(View.VISIBLE);
+            }else{
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+        mViewModel.getComicLiveData().observe(this, comic -> {
+            binding.comicNumberTextView.setText(String.valueOf(comic.getNum()));
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(comic.getTitle());
+            Glide.with(this).load(comic.getImgUrl()).into(binding.imageView);
+        });
+
         mViewModel.getMessageEventLiveData().observe(this, eventString -> {
             String message = eventString.getEventContentIfNotHandled();
             if (message != null){
@@ -49,12 +67,7 @@ public class ComicFragment extends Fragment {
         });
 
         mViewModel.loadComic();
-    }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FragmentComicBinding binding = FragmentComicBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 }
