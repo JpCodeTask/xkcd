f6c60139c92b77c734e6221a94482a5d3aa6711a
Jakub Pruszyński <jpcodetask@gmail.com>
2019-05-26 08:40:52 +0200

    basic setup for favorites

--- app/src/main/java/pl/jpcodetask/xkcdcomics/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/ui/favorites/FavoritesFragment.java
@@ -2,12 +2,15 @@ package pl.jpcodetask.xkcdcomics.ui.favorites;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import pl.jpcodetask.xkcdcomics.R;
 import pl.jpcodetask.xkcdcomics.databinding.FragmentFavoritesBinding;
 import pl.jpcodetask.xkcdcomics.databinding.FragmentFavoritesBindingImpl;
 
@@ -27,6 +30,20 @@ public class FavoritesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = FragmentFavoritesBindingImpl.inflate(inflater, container, false);
+
+        setupToolbar();
+        setHasOptionsMenu(true);
+
         return mBinding.getRoot();
+    }
+
+    private void setupToolbar() {
+        mBinding.toolbar.setTitle(R.string.nav_item_favorites);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mBinding.toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
