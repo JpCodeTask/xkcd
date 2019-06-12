a55f8d8863ec6cdccbbc692e0490881dae3fc57a
Jakub Pruszyński <jpcodetask@gmail.com>
2019-06-12 19:38:40 +0200

    setup default glide request options

--- app/src/main/java/pl/jpcodetask/xkcdcomics/utils/MyAppGlideModule.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/utils/MyAppGlideModule.java
@@ -1,8 +1,21 @@
 package pl.jpcodetask.xkcdcomics.utils;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 @GlideModule
 public class MyAppGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        super.applyOptions(context, builder);
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+    }
 }
