ea631253e7f613a49a9859327c01eb2c60226b74
Jakub Pruszyński <J.Pruszynski@itl.waw.pl>
2019-06-19 14:53:17 +0200

    fix bug with wrong settings index

--- app/src/main/java/pl/jpcodetask/xkcdcomics/ui/common/NavigationItem.java
+++ app/src/main/java/pl/jpcodetask/xkcdcomics/ui/common/NavigationItem.java
@@ -4,7 +4,7 @@ public interface NavigationItem {
 
     int NAVIGATION_EXPLORE = 0;
     int NAVIGATION_FAVORITES = 1;
-    int NAVIGATION_ARCHIVE  = 2;
-    int NAVIGATION_SETTINGS  = 3;
+    int NAVIGATION_ARCHIVE  = -1;
+    int NAVIGATION_SETTINGS  = 2;
     //int NAVIGATION_FEEDBACK = 4;
 }
